0d23a68ea3b113d65e3ed5eeae98e701ed3a7290
chae-ring <149919388+chae-ring@users.noreply.github.com>
2026-08-12 00:33:11 +0900

    feat: prd repository추가

--- backend/src/main/java/com/mcm/mcmoments/product/repository/ProductSerialRepository.java
+++ backend/src/main/java/com/mcm/mcmoments/product/repository/ProductSerialRepository.java
@@ -1,5 +1,12 @@
 package com.mcm.mcmoments.product.repository;
 
-public class ProductSerialRepository {
-    
+import com.mcm.mcmoments.product.entity.ProductSerial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductSerialRepository
+        extends JpaRepository<ProductSerial, Long> {
+
+    Optional<ProductSerial> findBySerialNumber(String serialNumber);
 }
--- backend/src/main/java/com/mcm/mcmoments/product/repository/UserProductRepository.java
+++ backend/src/main/java/com/mcm/mcmoments/product/repository/UserProductRepository.java
@@ -1,5 +1,10 @@
 package com.mcm.mcmoments.product.repository;
 
-public class UserProductRepository {
-    
+import com.mcm.mcmoments.product.entity.UserProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserProductRepository
+        extends JpaRepository<UserProduct, Long> {
+
+    boolean existsBySerial_Id(Long serialId);
 }
