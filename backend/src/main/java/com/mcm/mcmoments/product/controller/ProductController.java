6ef9c37ccf7d67b46873c4449e700327f7a04fe1
chae-ring <149919388+chae-ring@users.noreply.github.com>
2026-08-12 00:38:28 +0900

    feat: prdapi 추가

--- backend/src/main/java/com/mcm/mcmoments/product/controller/ProductController.java
+++ backend/src/main/java/com/mcm/mcmoments/product/controller/ProductController.java
@@ -1,5 +1,49 @@
 package com.mcm.mcmoments.product.controller;
 
+import com.mcm.mcmoments.product.dto.SerialVerifyRequest;
+import com.mcm.mcmoments.product.dto.SerialVerifyResponse;
+import com.mcm.mcmoments.product.dto.UserProductCreateRequest;
+import com.mcm.mcmoments.product.dto.UserProductCreateResponse;
+import com.mcm.mcmoments.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class ProductController {
-    
+
+    private final ProductService productService;
+
+    @PostMapping("/products/serial/verify")
+    public ResponseEntity<SerialVerifyResponse> verifySerial(
+            @RequestBody SerialVerifyRequest request
+    ) {
+
+        SerialVerifyResponse response =
+                productService.verifySerial(
+                        request.getSerialNumber()
+                );
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/user-products")
+    public ResponseEntity<UserProductCreateResponse> registerProduct(
+            Authentication authentication,
+            @RequestBody UserProductCreateRequest request
+    ) {
+
+        Long userId = (Long) authentication.getPrincipal();
+
+        UserProductCreateResponse response =
+                productService.registerProduct(
+                        userId,
+                        request
+                );
+
+        return ResponseEntity.ok(response);
+    }
 }
